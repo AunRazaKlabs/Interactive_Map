32426799813e5a2c191ebdc9e1e35667b6f72daf
AunRaza <aun.klabs@gmail.com>
2023-09-08 14:16:41 +0500

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -35,20 +35,32 @@ class InteractiveMap extends StatelessWidget {
       LatLng northEast = const LatLng(0, 0.0);
       LatLng southWest = const LatLng(90.0, 180.0);
 
+      double latitudePerPixel =
+          (southWest.latitude - northEast.latitude) / 3000.0;
+      double longitudePerPixel =
+          (southWest.longitude - northEast.longitude) / 3000.0;
+
+      int x = 2000; // Example pixel coordinate
+      int y = 2000;
+
+      double latitude = northEast.latitude + (y * latitudePerPixel);
+      double longitude = northEast.longitude + (x * longitudePerPixel);
+
       LatLngBounds latLngBonds = LatLngBounds(northEast, southWest);
       MapOptions mapOptions = MapOptions(
-        // center:  LatLng(0, northEast.longitude),
-        // onTap: (position, coordinates) {},
-        // crs: con,
-        // boundsOptions: FitBoundsOptions()
-        adaptiveBoundaries: false,
-        screenSize: Size(MediaQuery.of(context).size.width,
-            MediaQuery.of(context).size.height),
-        minZoom: 1,
-        maxZoom: 4,
-        bounds: latLngBonds,
-        // crs: CrsSimple(),/
-      );
+          center: LatLng(latitude, longitude),
+          // onTap: (position, coordinates) {},
+          // crs: con,
+          // boundsOptions: FitBoundsOptions()
+          // screenSize: Size(MediaQuery.of(context).size.width,
+          //     MediaQuery.of(context).size.height),
+          minZoom: 4,
+          maxZoom: 4,
+          zoom: 4,
+          maxBounds: latLngBonds
+          // bounds: latLngBonds,
+          // crs: CrsSimple(),/
+          );
 
       var overlayImages = <OverlayImage>[
         OverlayImage(
@@ -60,15 +72,53 @@ class InteractiveMap extends StatelessWidget {
           //     const AssetImage('assets/map_images/almasyyanmap-white2.jpg'),
         ),
       ];
+
       final filledPoints = <LatLng>[
-        const LatLng(30.5, 0.09), // Point A
-        const LatLng(
-            40.3498, 0.09), // Point B (Adjusting longitude to match point A)
-        const LatLng(
-            40.3498, 10.0), // Point C (Adjusting latitude to create a square)
-        const LatLng(
-            30.5, 10.0), // Point D (Adjusting longitude to match point A)
+        LatLng(latitude / 2049.5, longitude / 1568),
+        LatLng(latitude / 2048, longitude / 1584.5),
+        LatLng(latitude / 2028, longitude / 1621),
+        LatLng(latitude / 1982, longitude / 1697),
+        LatLng(latitude / 1947, longitude / 1746.5),
+        LatLng(latitude / 1924, longitude / 1750),
+        LatLng(latitude / 1916.75, longitude / 1756.5),
+        LatLng(latitude / 1921.03, longitude / 1779.46),
+        LatLng(latitude / 1858.56, longitude / 1845.93),
+        LatLng(latitude / 1803.53, longitude / 1896.97),
+        LatLng(latitude / 1763.5, longitude / 1901),
+        LatLng(latitude / 1715.5, longitude / 1870),
+        LatLng(latitude / 1673.5, longitude / 1844),
+        LatLng(latitude / 1604, longitude / 1799),
+        LatLng(latitude / 1562, longitude / 1772),
+        LatLng(latitude / 1429, longitude / 1687),
+        LatLng(latitude / 1423.5, longitude / 1651.5),
+        LatLng(latitude / 1543.5, longitude / 1489),
+        LatLng(latitude / 1564, longitude / 1485),
+        LatLng(latitude / 1572, longitude / 1475),
+        LatLng(latitude / 1569.92, longitude / 1453.6),
+        LatLng(latitude / 1691, longitude / 1301.5),
+        LatLng(latitude / 1692.75, longitude / 1300),
+        LatLng(latitude / 1696.5, longitude / 1298),
+        LatLng(latitude / 1700.75, longitude / 1297.25),
+        LatLng(latitude / 1707.25, longitude / 1296.5),
+        LatLng(latitude / 1713.25, longitude / 1298.25),
+        LatLng(latitude / 1727.5, longitude / 1309.75),
+        LatLng(latitude / 1769.25, longitude / 1342),
+
+        LatLng(latitude / 1817.5, longitude / 1379),
+        LatLng(latitude / 1942.25, longitude / 1475.5),
+        LatLng(latitude / 2043, longitude / 1553.75),
+        LatLng(latitude / 2048.25, longitude / 1559),
+        LatLng(latitude / 2049.25, longitude / 1563.75),
+        // const LatLng(3000/1572, 3000/1475),
+        // const LatLng(30.5, 0.09), // Point A
+        // const LatLng(
+        //     40.3498, 0.09), // Point B (Adjusting longitude to match point A)
+        // const LatLng(
+        //     40.3498, 20.0), // Point C (Adjusting latitude to create a square)
+        // const LatLng(
+        //     30.5, 20.0), // Point D (Adjusting longitude to match point A)
       ];
+
       return FlutterMap(
         options: mapOptions,
         nonRotatedChildren: [
@@ -86,9 +136,10 @@ class InteractiveMap extends StatelessWidget {
           PolygonLayer(
             polygons: [
               Polygon(
+                label: 'Test',
                 points: filledPoints,
                 isFilled: true,
-                color: Colors.purple,
+                color: Colors.red,
                 // borderColor: Colors.yellow,
                 // borderStrokeWidth: 4,
               ),
