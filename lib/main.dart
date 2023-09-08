4a60981afd06f94b9810f45c808b87ff6342ad6c
AunRaza <aun.klabs@gmail.com>
2023-09-08 19:17:45 +0500

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -29,38 +29,32 @@ class InteractiveMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     FlutterMap flutterMap() {
-      // double imageHeight = 3000.0;
-      // double imageWidth = 3000.0;
-
-      LatLng northEast = const LatLng(0, 0.0);
-      LatLng southWest = const LatLng(90.0, 180.0);
-
-      double latitudePerPixel =
-          (southWest.latitude - northEast.latitude) / 3000.0;
-      double longitudePerPixel =
-          (southWest.longitude - northEast.longitude) / 3000.0;
-
-      int x = 2000; // Example pixel coordinate
-      int y = 2000;
-
-      double latitude = northEast.latitude + (y * latitudePerPixel);
-      double longitude = northEast.longitude + (x * longitudePerPixel);
+      final Transformation transformation = Transformation();
+      LatLng northEast = transformation.transform(0, 0);
+      LatLng southWest = transformation.transform(3000, 3000);
 
       LatLngBounds latLngBonds = LatLngBounds(northEast, southWest);
       MapOptions mapOptions = MapOptions(
-          center: LatLng(latitude, longitude),
-          // onTap: (position, coordinates) {},
+          onTap: (position, coordinates) {
+            debugPrint(coordinates.toString());
+            debugPrint(position.global.toString());
+            debugPrint(position.relative.toString());
+          },
           // crs: con,
           // boundsOptions: FitBoundsOptions()
           // screenSize: Size(MediaQuery.of(context).size.width,
           //     MediaQuery.of(context).size.height),
-          minZoom: 4,
+          // screenSize: Size(
+          //   MediaQuery.of(context).size.width,
+          //   MediaQuery.of(context).size.height,
+          // ),
+          minZoom: 2,
           maxZoom: 4,
-          zoom: 4,
-          maxBounds: latLngBonds
+          zoom: 2,
+          maxBounds: latLngBonds,
           // bounds: latLngBonds,
-          // crs: CrsSimple(),/
-          );
+          center: transformation.transform(1500, 1500),
+          crs: const Epsg4326());
 
       var overlayImages = <OverlayImage>[
         OverlayImage(
@@ -73,76 +67,81 @@ class InteractiveMap extends StatelessWidget {
         ),
       ];
 
-      final filledPoints = <LatLng>[
-        LatLng(latitude / 2049.5, longitude / 1568),
-        LatLng(latitude / 2048, longitude / 1584.5),
-        LatLng(latitude / 2028, longitude / 1621),
-        LatLng(latitude / 1982, longitude / 1697),
-        LatLng(latitude / 1947, longitude / 1746.5),
-        LatLng(latitude / 1924, longitude / 1750),
-        LatLng(latitude / 1916.75, longitude / 1756.5),
-        LatLng(latitude / 1921.03, longitude / 1779.46),
-        LatLng(latitude / 1858.56, longitude / 1845.93),
-        LatLng(latitude / 1803.53, longitude / 1896.97),
-        LatLng(latitude / 1763.5, longitude / 1901),
-        LatLng(latitude / 1715.5, longitude / 1870),
-        LatLng(latitude / 1673.5, longitude / 1844),
-        LatLng(latitude / 1604, longitude / 1799),
-        LatLng(latitude / 1562, longitude / 1772),
-        LatLng(latitude / 1429, longitude / 1687),
-        LatLng(latitude / 1423.5, longitude / 1651.5),
-        LatLng(latitude / 1543.5, longitude / 1489),
-        LatLng(latitude / 1564, longitude / 1485),
-        LatLng(latitude / 1572, longitude / 1475),
-        LatLng(latitude / 1569.92, longitude / 1453.6),
-        LatLng(latitude / 1691, longitude / 1301.5),
-        LatLng(latitude / 1692.75, longitude / 1300),
-        LatLng(latitude / 1696.5, longitude / 1298),
-        LatLng(latitude / 1700.75, longitude / 1297.25),
-        LatLng(latitude / 1707.25, longitude / 1296.5),
-        LatLng(latitude / 1713.25, longitude / 1298.25),
-        LatLng(latitude / 1727.5, longitude / 1309.75),
-        LatLng(latitude / 1769.25, longitude / 1342),
-
-        LatLng(latitude / 1817.5, longitude / 1379),
-        LatLng(latitude / 1942.25, longitude / 1475.5),
-        LatLng(latitude / 2043, longitude / 1553.75),
-        LatLng(latitude / 2048.25, longitude / 1559),
-        LatLng(latitude / 2049.25, longitude / 1563.75),
-        // const LatLng(3000/1572, 3000/1475),
-        // const LatLng(30.5, 0.09), // Point A
-        // const LatLng(
-        //     40.3498, 0.09), // Point B (Adjusting longitude to match point A)
-        // const LatLng(
-        //     40.3498, 20.0), // Point C (Adjusting latitude to create a square)
-        // const LatLng(
-        //     30.5, 20.0), // Point D (Adjusting longitude to match point A)
+      // lng y axis
+      // final originalPoints1 = <LatLng>[
+      //   transformation.transform(0, 0),
+      //   transformation.transform(1500, 0),
+      //   transformation.transform(3000, 3000),
+      //   transformation.transform(0, 1500),
+      // ];
+      // final originalPoints2 = <LatLng>[
+      //   transformation.transform(2049.5, 1568),
+      //   transformation.transform(1982, 1697),
+      //   transformation.transform(1700.75, 1297.25),
+      //   transformation.transform(1572, 1475),
+      //   // transformation.transform(2028, 1621),
+      //   // transformation.transform(2049, 1568),
+      //   // transformation.transform(2049, 1568),
+      //   // transformation.transform(0, 3000),
+      // ];
+      // print(originalPoints2);
+      List<List<num>> polygon = [
+        [2049.5, 1568],
+        [2048, 1584.5],
+        [2028, 1621],
+        [1982, 1697],
+        [1947, 1746.5],
+        [1924.5, 1750],
+        [1916.75, 1756.5],
+        [1921.03, 1779.46],
+        [1858.56, 1845.93],
+        [1803.53, 1896.97],
+        [1763.5, 1901],
+        [1715.5, 1870],
+        [1673.5, 1844],
+        [1604, 1799],
+        [1562, 1772],
+        [1429, 1687],
+        [1423.5, 1651.5],
+        [1543.5, 1489],
+        [1564, 1485],
+        [1572, 1475],
+        [1569.92, 1453.6],
+        [1691, 1301.5],
+        [1692.75, 1300],
+        [1696.5, 1298],
+        [1700.75, 1297.25],
+        [1707.25, 1296.5],
+        [1713.25, 1298.25],
+        [1727.5, 1309.75],
+        [1769.25, 1342],
+        [1817.5, 1379],
+        [1942.25, 1475.5],
+        [2043, 1553.75],
+        [2048.25, 1559],
+        [2049.25, 1563.75]
       ];
-
+      List<LatLng> polygonLatLng = [];
+      for (var point in polygon) {
+        polygonLatLng.add(transformation.transform(point[0], point[1]));
+      }
       return FlutterMap(
         options: mapOptions,
         nonRotatedChildren: [
-          // TileLayer(
-          //   tileProvider: (),
-          //   maxZoom: 22,
-          //   backgroundColor: Colors.red,
-          // urlTemplate: 'assets/map_images/almasyyanmap-white2.jpg',
-          //   urlTemplate:
-          //       'https://cdn.pixabay.com/photo/2017/10/31/09/55/dream-job-2904780_1280.jpg',
-          // ),
           OverlayImageLayer(
             overlayImages: overlayImages,
           ),
           PolygonLayer(
             polygons: [
               Polygon(
-                label: 'Test',
-                points: filledPoints,
-                isFilled: true,
-                color: Colors.red,
-                // borderColor: Colors.yellow,
-                // borderStrokeWidth: 4,
-              ),
+                  label: 'Test',
+                  points: polygonLatLng,
+                  isFilled: true,
+                  color: Colors.red.withOpacity(0.4),
+                  labelStyle: const TextStyle(color: Colors.white)
+                  // borderColor: Colors.yellow,
+                  // borderStrokeWidth: 4,
+                  ),
             ],
           ),
         ],
@@ -150,62 +149,29 @@ class InteractiveMap extends StatelessWidget {
     }
 
     return flutterMap();
-    // return FlutterMap(
-    //   options: MapOptions(
-    //     center: const LatLng(26.24028, 50.65392),
-    //     onTap: (position, coordinates) {
-    //       debugPrint(position.toString());
-    //       debugPrint(coordinates.toString());
-    //     },
-    //     maxZoom: 22,
-    //     zoom: 22,
-    //   ),
-    //   children: [
-    //     TileLayer(
-    //       tileProvider: NetworkTileProvider(),
-    //       maxZoom: 22,
-    //       backgroundColor: Colors.red,
-    //       // urlTemplate: 'assets/map_images/almasyyanmap-white2.jpg',
-    //       urlTemplate:
-    //           'https://cdn.pixabay.com/photo/2017/10/31/09/55/dream-job-2904780_1280.jpg',
-    //     ),
-    //     OverlayImageLayer(
-    //       overlayImages: <OverlayImage>[
-    //         OverlayImage(
-    //           bounds: LatLngBounds(const LatLng(90, 150), const LatLng(90, 0)),
-    //           opacity: 1,
-    //           imageProvider:
-    //               const AssetImage('assets/map_images/almasyyanmap-white2.jpg'),
-    //         ),
-    //       ],
-    //     )
-    // TileLayer(
-    //   urlTemplate: 'assets/map_images/almasyyanmap-white2.jpg',
-    //   userAgentPackageName: 'com.example.app',
-    // ),
-    // CircleLayer(
-    //   circles: [
-    //     CircleMarker(
-    //       point: const LatLng(26.24028, 50.65392),
-    //       radius: 3,
-    //       useRadiusInMeter: true,
-    //     ),
-    //   ],
-    // ),
-    // PolygonLayer(
-    //   polygons: [
-    //     Polygon(
-    //       points: [
-    //         const LatLng(30, 40),
-    //         const LatLng(20, 50),
-    //         const LatLng(25, 45)
-    //       ],
-    //       color: Colors.blue,
-    //       isFilled: true,
-    //     ),
-    //   ],
-    // ),
-    //   ],
-    // );
+  }
+}
+
+class Transformation {
+  final num northEastLat = 90;
+  final num northEastLng = 180;
+  final num southWestLat = -90;
+  final num southWestLng = 0;
+  final num imageHeight = 3000;
+  final num imageWidth = 3000;
+
+  transform(num x, num y, {double scale = 1.0}) {
+    return LatLng(
+        southWestLat + (x / imageWidth) * (northEastLat - southWestLat),
+        southWestLng + (y / imageHeight) * (northEastLng - southWestLng));
+  }
+
+  untransform(num lat, num lng, {double scale = 1.0}) {
+    final latitudePerPixel = (northEastLat - southWestLat) / imageHeight;
+    final longitudePerPixel = (northEastLng - southWestLng) / imageWidth;
+    return [
+      ((lat - southWestLat) / latitudePerPixel).round(),
+      ((lng - southWestLng) / longitudePerPixel).round()
+    ];
   }
 }
